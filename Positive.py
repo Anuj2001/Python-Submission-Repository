68c368f1e8afefecd5ee7381e2c976294e6a7912
ANUJ PANDEY <2018034@iiitdmj.ac.in>
2020-04-04 23:41:59 +0530

    Update Positive.py

--- Positive.py
+++ Positive.py
@@ -1,5 +1,5 @@
 li=list(map(int,input("Enter a sequence of integers:").split()))
-print("Positive Integers are:",end="")
+print("\nPositive Integers are:",end="")
 n=len(li)
 for i in range(n):
     if(li[i]>0): # Cheking for positive integers.
