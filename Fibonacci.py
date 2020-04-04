b70b9f98072eb91f4afa7ba9c367366aec832657
ANUJ PANDEY <2018034@iiitdmj.ac.in>
2020-04-04 23:40:23 +0530

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,7 +2,7 @@ n=int(input("Enter the length of Fibonacci series:"))
 arr=[0]*n
 arr[0]=0 # first member of fibonacci series.
 arr[1]=1 # Second member of fibonacci series.
-print("Fibonacci series:")
+print("\nFibonacci series:",end="")
 for i in range(n):
     if(i<2):
         print(arr[i],end=" ") #Initial two members of fibonacci series.
